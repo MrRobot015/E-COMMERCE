ee4e48049802b8ef43eb4db531bc89d2767420f9
Mujtaba Salah <mujtabasalah07@gmail.com>
2021-12-01 04:21:12 +0200

    home page (#2)

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,38 +1,112 @@
 import styled from "styled-components";
-import { ArrowLeftOutlinedIcon, ArrowRightOutlinedIcon } from '@mui/icons-material';
+import {
+  ArrowLeftOutlinedIcon,
+  ArrowRightOutlinedIcon,
+} from "@mui/icons-material";
+import { sliderItems } from "../data";
+import { useState } from "react";
 
 const Container = styled.div`
   width: 100px;
   height: 100vh;
   display: flex;
   position: relative;
+  overflow: hidden;
 `;
 
 const Arrow = styled.div`
-    width: 50px;
-    height: 50px;
-    border-color: #fff7f7;
-    border-radius: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: absolute;
-    left: ${props=> props.direcation === "left" && "10px"};
-    right: ${props=> props.direcation === "right" && "10px"};;
-    top: 0;
-    bottom: 0;
-    margin: auto;
-    cursor: pointer;
-    
-`
+  width: 50px;
+  height: 50px;
+  border-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  left: ${(props) => props.direcation === "left" && "10px"};
+  right: ${(props) => props.direcation === "right" && "10px"};
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  cursor: pointer;
+  z-index: 2;
+`;
+
+const Wrapper = styled.div`
+  height: 100px;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${props => props.slideIndex * 100} vw);
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: #${(props) => props.bg};
+`;
+
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Description = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
 
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direcation) => {
+    if (direcation === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
+
   return (
     <Container>
-      <Arrow direcation="left">
-        <ArrowLeftOutlinedIcon/>
+      <Arrow direcation="left" onClick={() => handleClick("left")}>
+        <ArrowLeftOutlinedIcon />
       </Arrow>
-      <Arrow direcation="right">
+      <Wrapper slideIndex = {slideIndex}>
+        {sliderItems.map((item) => (
+          <Slide bg={item.bg} key={item.id}>
+            <ImgContainer>
+              <Image src={item.img} />
+            </ImgContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Description>{item.desc}</Description>
+              <Button>SHOW NOW</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow direcation="right" onClick={() => handleClick("right")}>
         <ArrowRightOutlinedIcon />
       </Arrow>
     </Container>
