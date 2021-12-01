4d32c8bce1d4a1fb7bfcf54b00c16873f1092260
Mujtaba Salah <mujtabasalah07@gmail.com>
2021-12-01 04:53:47 +0200

    home page (#3)

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -94,7 +94,7 @@ const Slider = () => {
       </Arrow>
       <Wrapper slideIndex = {slideIndex}>
         {sliderItems.map((item) => (
-          <Slide bg={item.bg} key={item.id}>
+          <Slide bg={item.bg}>
             <ImgContainer>
               <Image src={item.img} />
             </ImgContainer>
